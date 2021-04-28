7fe0309e0a37c95c989ab91a963188084b0ec2ec
oldzhg <zcy000321@gmail.com>
2021-04-28 13:57:43 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'oldzhg\'s blog',
+  author: 'oldzhg',
+  email: 'i@oldzhg.com',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
   lang: 'en-US',
